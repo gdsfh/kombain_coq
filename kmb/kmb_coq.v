25674eaef4344495c5c698e108fe62a3e7d38506
Dmitry Grebeniuk <gdsfh1+umodni@gmail.com>
2012-04-01 20:35:26 +0300

    modified proofs; more Hints use

--- kmb/kmb_coq.v
+++ kmb/kmb_coq.v
@@ -1,3 +1,5 @@
+Set Default Timeout 30.
+
 (* http://coq.inria.fr/cocorico/AUGER_ExtractionTuto : *)
 Require ExtrOcamlBasic.
 Require ExtrOcamlBigIntConv.
@@ -84,6 +86,9 @@ Definition int_zero := ExtrOcamlIntConv.int_zero.
       (int_le int_zero (int_sub b a)) = true
     .
 
+    Hint Resolve int_sub_le_zero : oc_int.
+    Hint Rewrite <- minus_rev_le : oc_int.
+
   End NoOverflow.
 
 
@@ -108,6 +113,36 @@ Definition int_zero := ExtrOcamlIntConv.int_zero.
     (int_sub a b) = (int_sub a c) ->
     b = c
   .
+
+  Axiom eq_of_sub_eq_rewr :
+    forall a b c : int,
+    ((int_sub a b) = (int_sub a c)) =
+    (b = c)
+  .
+
+  Axiom int_le_refl :
+    forall a : int,
+    int_le a a = true
+  .
+
+  Hint Resolve eq_of_nat_eq eq_of_sub_eq int_le_refl
+               nat_le_of_int
+    : oc_int.
+  Hint Rewrite eq_of_sub_eq_rewr : oc_int.
+
+  Axiom int_eq_to_nat_eq :
+    forall a b : int,
+    (int_le int_zero a) = true ->
+    (int_le int_zero b) = true ->
+    (int_eq a b) = true ->
+    nat_of_int_pos a = nat_of_int_pos b.
+
+  Axiom int_neq_to_nat_neq :
+    forall a b : int,
+    (int_le int_zero a) = true ->
+    (int_le int_zero b) = true ->
+    (int_eq a b) = false ->
+    nat_of_int_pos a <> nat_of_int_pos b.
 
 End OcamlInt.
 
@@ -193,6 +228,11 @@ Axiom class_le_x_x :
   forall A, forall (x : A), forall (cmpa : Cmp A),
   class_le x x = true.
 
+  Hint Unfold class_sub sub OcamlInt_Arith
+              int_Cmp class_le le class_eq
+              sub eq nat_Arith
+   : unf_oc eq_to_prop.
+
 End OcamlCmp.
 
 Notation "x == y" := (OcamlCmp.class_eq x y) (at level 70, no associativity).
@@ -249,21 +289,26 @@ Implicit Arguments Parsed [A].
 Implicit Arguments Failed [A].
 
 
-Section input_axioms.
-
-Variable inp : input.
-
 Axiom input_high_bound :
-  (input_pos inp <== input_len inp) = true
+  forall inp : input,
+  (int_le (input_pos inp) (input_len inp)) = true
 .
+
+Hint Resolve input_high_bound : inp.
 
 Axiom input_pos_low_bound :
-  (int_zero <== input_pos inp) = true
+  forall inp : input,
+  (int_le int_zero (input_pos inp)) = true
 .
 
+Hint Resolve input_pos_low_bound : inp.
+
 Axiom input_len_low_bound :
-  (int_zero <== input_len inp) = true
+  forall inp : input,
+  (int_le int_zero (input_len inp)) = true
 .
+
+Hint Resolve input_len_low_bound : inp.
 
 Axiom input_len_const :
   forall (A : Type),
@@ -276,40 +321,66 @@ Axiom input_len_const :
 .
 
 
-Definition input_left : int
+Definition input_left
  :=
+   Eval compute in
+   fun inp : input =>
    (input_len inp) - (input_pos inp)
 .
 
+Hint Unfold input_left : inp.
+
+
 Theorem input_left_low_bound :
-  (int_zero <== input_left) = true
+  forall inp : input,
+  (int_le int_zero (input_left inp)) = true
 .
-unfold input_left.
-apply int_sub_le_zero.
-exact input_high_bound.
+autounfold with inp; auto with oc_int inp.
 Defined.
 
-Definition input_left_nat : nat
- :=
-   nat_of_int_pos input_left
+Hint Resolve input_left_low_bound : inp.
+
+
+Definition input_left_nat
+  := fun (inp : input) =>
+       nat_of_int_pos (input_left inp)
 .
+
+Hint Unfold input_left_nat : inp.
+
 
 Definition input_left_pos :
-  int_le int_zero (input_len inp - input_pos inp) = true.
-apply int_sub_le_zero.
-apply input_high_bound.
+  forall inp : input,
+  int_le int_zero (int_sub (input_len inp) (input_pos inp)) = true.
+intros; auto with inp oc_int.
 Defined.
 
-End input_axioms.
+Hint Resolve input_left_pos : inp.
 
-Hint Resolve
-  input_high_bound
-  input_pos_low_bound
-  input_len_low_bound
-  input_left_low_bound
-  input_left_pos
-  : inp.
 
+Ltac t_lens :=
+  repeat (
+  match goal with
+    | [ H : (input_len ?a = input_len ?b) |- _ ] =>
+          rewrite H
+    | [ H : (input_len ?a = input_len ?b),
+        G : context [input_len ?a] |- _ ] =>
+          rewrite H in G
+  end
+  )
+.
+
+Ltac t_poss :=
+  try (
+  match goal with
+    | [ H : (input_pos ?a = input_pos ?b) |- _ ] =>
+          rewrite H
+    | [ H : (input_pos ?a = input_pos ?b),
+        G : context [input_pos ?a] |- _ ] =>
+          rewrite H in G
+  end
+  )
+.
 
 Lemma input_left_of_pos :
   forall i1 i2 : input,
@@ -317,13 +388,11 @@ Lemma input_left_of_pos :
   input_pos i1 = input_pos i2 ->
   input_left_nat i1 = input_left_nat i2
 .
-intros i1 i2 Hlen Hpos.
-unfold input_left_nat.
-unfold input_left.
-rewrite Hlen.
-rewrite Hpos.
-reflexivity.
+intros; autounfold with inp; t_lens; t_poss; auto.
 Defined.
+
+Hint Resolve input_left_of_pos : inp.
+
 
 Lemma input_pos_of_left :
   forall i1 i2 : input,
@@ -331,18 +400,12 @@ Lemma input_pos_of_left :
   input_left_nat i1 = input_left_nat i2 ->
   input_pos i1 = input_pos i2
 .
+repeat autounfold with unf_oc inp.
 intros i1 i2 Hlen Hleft.
-unfold input_left_nat in Hleft.
-unfold input_left in Hleft.
-apply eq_of_nat_eq in Hleft.
-rewrite Hlen in Hleft.
-unfold eq in Hleft.
-apply eq_of_sub_eq in Hleft.
-exact Hleft.
-auto with inp.
-auto with inp.
+apply eq_of_nat_eq in Hleft ; auto with oc_int inp.
+t_lens.
+autorewrite with oc_int in Hleft; auto.
 Defined.
-
 
 
 Definition nondecr
@@ -354,14 +417,19 @@ Definition nondecr
   forall i_before,
   match cond i_before with
     | Parsed _ i_after =>
-        (input_pos i_before <== input_pos i_after) = true
+        (int_le (input_pos i_before) (input_pos i_after)) = true
     | Failed => True
   end
 .
 
+Hint Unfold nondecr : par.
+
 
 Inductive par (A : Type) (pa : input -> result A) : Prop :=
   | Par : nondecr pa -> par A pa.
+
+Hint Constructors par : par.
+
 
 Ltac t_par :=
   repeat
@@ -386,8 +454,11 @@ Ltac t_par :=
 
 Definition ret := fun {A} (a : A) input => Parsed a input.
 
+Hint Unfold ret : par.
+
+
 Definition par_ret : forall A, forall (a : A), par A (ret a).
-intros; unfold ret; t_par.
+autounfold with par; auto with oc_int inp par.
 Qed.
 
 
@@ -418,32 +489,6 @@ Definition predicate_and :=
 Definition predicate_and' :=
   fun {A} (cond : input -> result A) inp =>
     predicate_not (fun inp' => predicate_not cond inp') inp
-.
-
-
-Ltac t_pass_par__ :=
-(*  repeat *)
-    (match goal with
-     | [ H : par _ ?cond |- context Q
-              [match (?cond ?input) with
-               | Parsed _ _ => _
-               | Failed => _
-               end
-              ]
-       ] =>
-           apply H in Q
-     end
-    )
-.
-
-
-Ltac t_lens :=
-  try (
-  match goal with
-    | [ H : (input_len ?a = input_len ?b) |- _ ] =>
-          rewrite H
-  end
-  )
 .
 
 
@@ -498,7 +543,7 @@ Theorem and_eq_and'
  : forall (cond : _ -> result unit) inp,
    predicate_and cond inp = predicate_and' cond inp
 .
-intros; compute; t_result; t_unit; auto.
+intros; compute. t_result.
 Qed.
 
 
@@ -666,10 +711,6 @@ Definition alt :=
 .
 
 
-Hint Unfold class_sub sub OcamlInt_Arith
-            int_Cmp class_le le class_eq
- : unf_oc eq_to_prop.
-
 Ltac unfold_oc :=
  repeat (
   autounfold with unf_oc; auto
@@ -688,16 +729,11 @@ Definition input_left_nonincr :
     | Failed => True
   end
 .
-intros.
-unfold input_left.
-t_result.
-unfold_oc.
-rewrite <- minus_rev_le.
-exact nondecr_input_cond.
+repeat autounfold with inp unf_oc. intros. t_result.
+autorewrite with oc_int.
+auto with oc_int.
 Defined.
 
-
-Hint Unfold input_left_nat input_left : inp.
 
 Theorem input_left_nat_nonincr :
   forall (A : Type),
@@ -710,19 +746,79 @@ Theorem input_left_nat_nonincr :
        input_left_nat input' <= input_left_nat input
    end)
 .
-autounfold with inp.
+autounfold with inp par.
 intros.
 t_result.
-unfold_oc.
-apply nat_le_of_int; unfold_oc.
+
+apply nat_le_of_int; auto with oc_int inp par.
 apply int_sub_le_zero.
-apply input_high_bound.
-apply int_sub_le_zero.
-rewrite <- input_len_const_input0.
-apply input_high_bound.
-rewrite <- minus_rev_le.
-exact nondecr_input_cond.
+rewrite <- input_len_const_input0 ; apply input_high_bound.
+
+autorewrite with oc_int; auto.
 Qed.
+
+
+(* working with input buffers on natural numbers *)
+
+Definition input_pos_nat := fun inp =>
+  nat_of_int_pos (input_pos inp).
+
+(* Hint Resolve int_neq_to_nat_neq : to_int. *)
+
+Definition input_pos_neq_to_nat :
+  forall i1 i2 : input,
+  int_eq (input_pos i1) (input_pos i2) = false ->
+  (input_pos_nat i1 <> input_pos_nat i2)
+.
+intros i1 i2.
+apply int_neq_to_nat_neq; auto with inp.
+Defined.
+
+Hint Extern 4 => apply nat_le_of_int : to_nat.
+(* Hint Resolve nat_le_of_int : inp. *)
+
+Definition input_pos_le_to_nat :
+  forall i1 i2 : input,
+  int_le (input_pos i1) (input_pos i2) = true ->
+  input_pos_nat i1 <= input_pos_nat i2.
+auto with inp to_nat.
+Defined.
+
+Definition input_len_nat :=
+  fun inp =>
+    nat_of_int_pos (input_len inp).
+
+Definition input_len_to_nat :
+  forall i1 i2 : input,
+  input_len i1 = input_len i2 ->
+  input_len_nat i1 = input_len_nat i2.
+intros; unfold input_len_nat; f_equal; auto.
+Defined.
+
+Definition input_left_nat_of_nats :
+  forall i : input,
+  input_left_nat i = input_len_nat i - input_pos_nat i.
+intros.
+repeat autounfold with unf_oc oc_int.
+unfold input_left_nat.
+unfold input_left.
+unfold input_len_nat.
+unfold input_pos_nat.
+apply minus_nat_of_int; auto with inp.
+Defined.
+
+Definition pos_plus_left_eq_len :
+  forall i,
+  input_pos_nat i + input_left_nat i = input_len_nat i.
+intros.
+rewrite input_left_nat_of_nats.
+repeat autounfold with unf_oc.
+rewrite Minus.le_plus_minus_r; auto with inp.
+
+assert (H := input_high_bound i).
+apply nat_le_of_int in H; auto with inp.
+Defined.
+
 
 
 Require Import Recdef.
@@ -748,41 +844,30 @@ Function star
       | Failed => Parsed tt input1
     end
 .
+
+repeat autounfold with unf_oc.
+
 intros.
-pose proof
-  (input_left_nat_nonincr A cond par0
-     input1)
-  as Hnonincr
-.
-rewrite teq in Hnonincr.
-pose proof
-  (input_len_const A cond input1)
-  as Hconstlen
-.
-rewrite teq in Hconstlen.
+apply input_pos_neq_to_nat in teq0.
 
-autounfold with unf_oc in teq0.
+t_par.
+assert (Hnd := nondecr_cond input1).
+rewrite teq in Hnd.
 
-autounfold with eq_to_prop in teq0.
-rewrite int_neq_to_prop in teq0.
+apply input_pos_le_to_nat in Hnd.
 
-pose proof
-  (input_pos_of_left input1 input2 Hconstlen)
-  as Heqleft.
+assert (Hconst := input_len_const A cond input1).
+rewrite teq in Hconst.
 
-assert (Hleftnoneq := fun w => teq0 (Heqleft w)).
+apply input_len_to_nat in Hconst.
 
-assert (Hdec := Compare_dec.le_lt_eq_dec
-  (input_left_nat input2)
-  (input_left_nat input1)
-  Hnonincr
-).
+repeat rewrite input_left_nat_of_nats.
+repeat autounfold with unf_oc.
 
-destruct Hdec. exact l.
+assert (Sum1 := pos_plus_left_eq_len input1).
+assert (Sum2 := pos_plus_left_eq_len input2).
 
-apply eq_sym in e.
-
-destruct (Hleftnoneq e).
+omega.
 
 Defined.
 
